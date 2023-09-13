a5bbff0e38d1c3173f31d217ed49e21763b3c655
Tây Vo Tuan <votuantay@gmail.com>
2023-09-13 09:58:57 +0200

    Formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import '../app.postcss'
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton'
 
 	// Floating UI for Popups
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
-	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom'
+	import { storePopup } from '@skeletonlabs/skeleton'
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow })
 </script>
 
 <!-- App Shell -->
