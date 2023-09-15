6ca5c346253e0581b164d1432de9f7ba3a9c6633
Tây Vo Tuan <votuantay@gmail.com>
2023-09-15 16:01:57 +0200

    discord auth in lib

--- src/lib/server/lucia.ts
+++ src/lib/server/lucia.ts
@@ -3,6 +3,9 @@ import { prisma } from '@lucia-auth/adapter-prisma'
 import { PrismaClient } from '@prisma/client'
 import { lucia } from 'lucia'
 import { sveltekit } from 'lucia/middleware'
+import { discord } from '@lucia-auth/oauth/providers'
+import { DISCORD_APP_ID, DISCORD_APP_SECRET } from '$env/static/private'
+import { hostname } from 'os'
 
 const client = new PrismaClient()
 
@@ -16,4 +19,11 @@ export const auth = lucia({
 			discordUserId: data.discord_id
 		}
 	}
+})
+
+export const discordAuth = discord(auth, {
+	clientId: DISCORD_APP_ID,
+	clientSecret: DISCORD_APP_SECRET,
+	redirectUri: hostname() + '/login/discord/callback',
+	scope: ['identify', 'guilds', 'guilds.member.read']
 })
