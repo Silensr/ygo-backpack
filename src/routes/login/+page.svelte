72654dc4e60ffd9a78732f11988b86c7020d0f4a
Tây Vo Tuan <votuantay@gmail.com>
2023-09-15 18:50:48 +0200

    Html elements for connection

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,4 @@
+<div class="h-full flex flex-col justify-center items-center">
+	<span class="my-8">Merci de vous connecter depuis votre compte de messagerie (Discord)</span>
+	<a href="/login/discord" class="btn variant-filled">Se connecter</a>
+</div>
