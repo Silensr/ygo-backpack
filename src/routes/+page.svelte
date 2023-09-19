eedca0b1babf18eeeea06d2923e29f4a5b2027bf
Tây Vo Tuan <votuantay@gmail.com>
2023-09-19 12:11:31 +0200

    logout button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
-<div class="h-full flex flex-col justify-center">
+<div class="h-full flex flex-col justify-center items-center">
 	<h1 class="h1 text-center">Bienvenue dans l'Intranet de la Duel Academy</h1>
+	<a href="/logout" class="btn variant-filled my-8">Déconnexion</a>
 </div>
